af5838395edb304f8f785b064b6d9e6a91b3323b
Cristian Ochoa <cristian8a60@gmail.com>
2025-02-25 20:56:05 +0100

    repositorio python

---
+++ Ejemplos_Python.py
@@ -0,0 +1,28 @@
+# Definimos una variable x con una cadena
+x = "El valor de (a+b)*c es"
+# Podemos realizar múltiples asignaciones
+a, b, c = 4, 3, 5
+# Realizamos unas operaciones con a,b,c
+d = (a + b) * c
+# Definimos una variable booleana
+imprimir = True
+# Si imprimir, print()
+if imprimir:
+ print(x, d)
+# Salida: El valor de (a+b)*c es 35
+
+
+import math
+math.pi * 4
+math.pi ** 2
+math.sqrt(4)     # Raíz cuadrada
+math. sqrt(12)
+
+
+import random
+random.random ()
+lista = [1, 2, 3, 4]
+random.choice(lista)
+random.shuffle(lista)
+lista
+
