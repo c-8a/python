e68b749c7890b6b8aef433e153ad89ce7cbd93e1
Cristian Ochoa <cristian8a60@gmail.com>
2025-03-12 19:55:33 +0100

    modificacion en Excepciones

--- Excepciones.py
+++ Excepciones.py
@@ -36,3 +36,78 @@ except IndexError: # Captura IndexError
 except TypeError: # Captura TypeError
     print('El índice tiene que ser un número.')
 print('Hemos salido del try-catch')
+
+a = 10
+b = 1000
+assert(a > b), 'A tiene que ser mayor que B!' # Si se cumple no salta el error
+print('Si se muestra esto es que no ha saltado el assert')
+
+a = 10
+b = 0
+c = 5
+assert(a == c), 'A y C tienen que ser iguales!'
+
+class miPropiaExcepcion(Exception): #Las excepciones heredan de Exception
+    pass
+try:
+    raise miPropiaExcepcion
+except miPropiaExcepcion:
+    print('He capturado mi propia excepción')
+
+
+class miError(Exception):
+    def __init__(self, valor):
+        self.valor = valor
+    def __str__(self):
+        return str(self.valor)
+    
+raise(miError('Mensaje de error'))
+
+
+def indexador(objeto, indice):
+    return objeto[indice]
+
+try:
+    indexador('Python', 10)
+finally:
+    print('Esto se ejecuta includo cuando salta la excepción')
+
+
+def indexador(objeto, indice):
+    return objeto[indice]
+try:
+    indexador('Python', 10)
+    print('Este print no se ejecuta')
+finally:
+    print('Esto se ejecuta includo cuando salta la excepción')
+
+def indexador(objeto, indice):
+    return objeto[indice]
+try:
+    indexador('Python', 10)
+finally:
+    print('Esto se ejecuta includo cuando salta la excepción')
+print('Este print tampoco se ejecuta') 
+
+def indexador(objeto, indice):
+    return objeto[indice]
+try:
+    indexador('Python', 10)
+except IndexError:
+    print('Capturamos la excepción')
+finally:
+    print('Esto se ejecuta incluso cuando salta la excepción')
+print('Se ejecutará este print?') 
+
+def divide(x, y):
+    try:
+        resultado = x/y
+    except ZeroDivisionError:
+        print('No se puede dividir por cero')
+    else:
+        print('El resultado es: ', resultado)
+    finally:
+        print('Ejecutamos el finally')
+divide(4, 12)
+divide(4, 0)
+
