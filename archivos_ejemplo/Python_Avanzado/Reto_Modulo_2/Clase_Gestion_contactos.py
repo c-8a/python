38ec06d7b38bbdb51df2457f263509f97ce7637a
Cristian Ochoa <cristian8a60@gmail.com>
2025-04-15 20:49:36 +0200

    Se completa el codigo del Reto 2

--- archivos_ejemplo/Python_Avanzado/Reto_Modulo_2/Clase_Gestion_contactos.py
+++ archivos_ejemplo/Python_Avanzado/Reto_Modulo_2/Clase_Gestion_contactos.py
@@ -29,6 +29,7 @@ class GestionContactos:
     def buscar_contacto(self, nombre):
         encontrado = False
         contador = 0
+        localizacion = []
         try:
             with open(self.nombre_archivo, 'r', encoding='utf-8') as archivo:
                 for i, linea in enumerate(archivo):
@@ -38,34 +39,46 @@ class GestionContactos:
                         print(f"Telefono: {next(archivo)}", end = "")
                         print(f"Email: {next(archivo)}")
                         encontrado = True
+                        localizacion.append(i)
                 if not encontrado:
                     print(f"Contacto \"{nombre}\" no encontrado en la agenda")
                 else:
                     print(f"{contador} contactos encontrados en la agenda")
+                    return localizacion
         except Exception as e:
             print(f"Error al abrir o leer el archivo {e}")
 
 
     def eliminar_contacto(self, nombre):
-        encontrado = False
-        try:
-            with open(self.nombre_archivo, 'r', encoding='utf-8') as archivo:
-                for i, linea in enumerate(archivo):
-                    if nombre.lower() in linea.lower():
-                        print(f"Nombre: {linea}", end = "")
-                        print(f"Telefono: {next(archivo)}", end = "")
-                        print(f"Email: {next(archivo)}")
-                        encontrado = True
-                        break
-                if not encontrado:
-                    print(f"Contacto \"{nombre}\" no encontrado en la agenda")
-                else:
-                    print("¿Está seguro de querer eliminar el contacto?")
-                    if input("Y | N") == "Y":
-                        pass
-                    else:
-                        return
-        except Exception as e:
-            print(f"Error al abrir o leer el archivo {e}")
+        localizacion = self.buscar_contacto(nombre)
+        if len(localizacion) == 0:
+            return
+        elif len(localizacion) == 1:
+            print("¿Está seguro de querer eliminar el contacto?")
+            if input("Y | N: ").lower() == "y":
+                try:
+                    with open(self.nombre_archivo, 'r', encoding='utf-8') as archivo:
+                        lineas = archivo.readlines()
+
+                    i = 0
+                    lineas_filtradas = []
+                    while i < len(lineas):
+                        if i not in localizacion:
+                            lineas_filtradas.append(lineas[i])
+                            i += 1  # Pasar a la siguiente línea
+                        else:
+                            i += 4  # Saltar las siguientes 4 líneas (incluida la actual)
+
+                    with open(self.nombre_archivo, 'w', encoding='utf-8') as archivo:
+                        archivo.writelines(lineas_filtradas)
+
+                    print(f"Se eliminó el contacto {nombre} de la agenda.")
+
+                except Exception as e:
+                    print(f"Error al procesar el archivo: {e}")
+            else:
+                return
+        else:
+            print("Demasiados contactos encontrados, intente de nuevo")
 
 
